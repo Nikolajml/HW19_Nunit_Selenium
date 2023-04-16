5e130392d0865e8093bdd6d30b4346e9c2982598
Nikolajmn <nicolas.maliavko@gmail.com>
2023-04-16 20:58:11 +0500

    Add project files.

---
+++ Nunit_Selenium/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Nunit_Selenium
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
