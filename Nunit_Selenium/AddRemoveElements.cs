c6efaa79714cff3efaffe23db2e1a6a5352fed2f
Nikolajmn <nicolas.maliavko@gmail.com>
2023-04-17 14:09:11 +0500

    Added test A - add and remove elements

--- Nunit_Selenium/AddRemoveElements.cs
+++ Nunit_Selenium/AddRemoveElements.cs
@@ -17,14 +17,30 @@ namespace Nunit_Selenium
         }
 
         [Test]
-        public void Test_A_AddRemoveElements()
+        public void Test_A_Add_Remove_Elements()
         {
-            ChromeDriver.Navigate().GoToUrl("http://the-internet.herokuapp.com/add_remove_elements/");            
+            ChromeDriver.Navigate().GoToUrl("http://the-internet.herokuapp.com/add_remove_elements/");
+                        
+            IWebElement element = ChromeDriver.FindElement(By.XPath("//button[text()='Add Element']"));
+            element.Click();
+            element.Click();            
+
+            ChromeDriver.FindElement(By.XPath("//button[text()='Delete']")).Click();
+
+            var elements = ChromeDriver.FindElements(By.XPath("//button[text()='Delete']"));
+
+            foreach (var e in elements)
+            {
+                Console.WriteLine(e);
+            }
+
+            Assert.True(elements.Any());
         }
 
         [TearDown]
         public void TearDown()
         {
+            Console.WriteLine("Test completed");
             ChromeDriver.Quit();
         }
     }
