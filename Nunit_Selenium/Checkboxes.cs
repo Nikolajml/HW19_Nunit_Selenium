9af6233d3b50e73ae84d6f3adc2dadbc6a749fba
Nikolajmn <nicolas.maliavko@gmail.com>
2023-04-17 17:20:38 +0500

    Added test B - Checkboxes

---
+++ Nunit_Selenium/Checkboxes.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Nunit_Selenium
+{  
+    [TestFixture]
+    public class Checkboxes
+    {
+        WebDriver ChromeDriver { get; set; }
+
+        [SetUp]
+        public void Setup() 
+        {
+            ChromeDriver = new ChromeDriver();
+            ChromeDriver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void Test_B_Checkboxes() 
+        {
+            ChromeDriver.Navigate().GoToUrl("http://the-internet.herokuapp.com/checkboxes");                        
+                        
+            List<IWebElement> elements = ChromeDriver.FindElements(By.CssSelector("[type=checkbox]")).ToList();            
+            var checkBox1_Attribut1 = elements[0].GetAttribute("");
+            Assert.IsNull(checkBox1_Attribut1);
+
+            elements[0].Click();
+            var checkBox1_Attribut2 = elements[0].GetAttribute("checked");
+            Assert.IsNotNull(checkBox1_Attribut2);
+
+            var checkBox2_Attribut1 = elements[1].GetAttribute("checked");
+            Assert.IsNotNull(checkBox2_Attribut1);
+
+            elements[1].Click();
+            var checkBox2_Attribut2 = elements[1].GetAttribute("");
+            Assert.IsNull(checkBox2_Attribut2);
+        }
+
+        [TearDown] 
+        public void TearDown() 
+        { 
+            ChromeDriver.Quit();
+        }
+    }
+}
---
+++ Nunit_Selenium/Dropdown.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nunit_Selenium
+{
+    [TestFixture]
+    internal class Dropdown
+    {
+        WebDriver ChromeDriver { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            ChromeDriver = new ChromeDriver();
+            ChromeDriver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void Test_C_Dropdown()
+        {
+            
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            ChromeDriver.Quit();
+        }
+    }
+}
